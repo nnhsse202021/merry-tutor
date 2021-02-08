22aa27ea3d591384a76ca7ad9e300ff83c65dbc5
cyandev <djs.bitcoin@gmail.com>
2021-02-08 14:29:52 -0600

    add mongodb for users

--- app.js
+++ app.js
@@ -6,14 +6,30 @@ app.set("view engine", "ejs"); //use EJS for view engine
 app.use(express.static("public")) //static files served from /public/, (eg. url.com/img/example.png serves from /public/img/example.png)
 
 let session = require('express-session');
-app.use(session({secret: "Shh, its a secret!"})); //sessions
+app.use(session({secret: "Shh, its a secret!"})); //REPLACE THE SECRET WITH SOMETHING SECURE LATER
 
 let bodyParser = require("body-parser");
-app.use(bodyParser.json()) //body parser
+app.use(bodyParser.json()); //body parser
+
+const MongoClient = require('mongodb').MongoClient;
+const uri = `mongodb+srv://admin:${process.env.MONGO_PASSWORD}@cluster0.kfvlj.mongodb.net/merry-tutor?retryWrites=true&w=majority`;
+const mongoClient = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+//middleware to attach user data to all requests
+let usersCollection;
+mongoClient.connect(err => {
+    if (err) console.log(err);
+    usersCollection = mongoClient.db("merry-tutor").collection("Users");
+    console.log("Auth Connected to Users collection");
+});
+app.use(async (req, res, next) => {
+    req.user = await usersCollection.findOne({_id: req.session.userId});
+    next()
+})
 
 //routes
 app.use("/student", require("./routes/student.js")); //anything send to /student... will be sent to student.js
-app.use("/tutor", require("./routes/tutor.js"))
+app.use("/tutor", require("./routes/tutor.js"));
 app.use("/auth", require("./routes/auth.js"));
 
 app.get("/", (req,res) => {
@@ -21,7 +37,7 @@ app.get("/", (req,res) => {
 })
 
 app.get("/login", (req,res) => {
-    res.render("login")
+    res.render("login");
 })
 
-app.listen(8080, () => {console.log("Listening on port 8080")})
+app.listen(8080, () => {console.log("Listening on port 8080")});
--- routes/auth.js
+++ routes/auth.js
@@ -1,30 +1,91 @@
 let express = require("express");
+const CLIENT_ID = "1037005622588-7dul28gjauau6d2b3572ue7vq9cgggcc.apps.googleusercontent.com"
 
 let {OAuth2Client} = require('google-auth-library');
-let client = new OAuth2Client(process.env.CLIENT_ID);
+let oAuth2Client = new OAuth2Client(CLIENT_ID);
 
+const MongoClient = require('mongodb').MongoClient;
+const uri = `mongodb+srv://admin:${process.env.MONGO_PASSWORD}@cluster0.kfvlj.mongodb.net/merry-tutor?retryWrites=true&w=majority`;
+const mongoClient = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+let usersCollection;
+mongoClient.connect(err => {
+    if (err) console.log(err);
+    usersCollection = mongoClient.db("merry-tutor").collection("Users");
+    console.log("Auth-Middleware Connected to Users collection");
+});
+
+/*
+User Data Schema:
+{
+    _id: "string",
+    name: {
+        first: "First",
+        last: "Last"
+    },
+    email: "example@gmail.com", (optional)
+    google_sub: "string" (optional)
+    roles: ["tutee", "parent", "tutor", "board"],
+    children: ["id1", "id2"],
+}
+*/
 router = express.Router();
 
 router.post("/v1/google", async (req, res) => {
-    let { token }  = req.body
-    let ticket = await client.verifyIdToken({
+    let { token }  = req.body;
+    let ticket = await oAuth2Client.verifyIdToken({
         idToken: token,
-        audience: process.env.CLIENT_ID
+        audience: CLIENT_ID
     });
-    let {sub, email, name} = ticket.getPayload();
+    console.log(ticket.getPayload());
+    let {sub, email, given_name, family_name} = ticket.getPayload();
     //do something with the payload and get internal userId, using sub as a replacement for now
-    req.session.userId = sub;
-    res.status(201)
-    res.json({})
+    let user = getOrMakeUser(sub, email, given_name.toLowerCase(), family_name.toLowerCase());
+    req.session.userId = user._id;
+    res.status(201);
+    res.json({});
 })
 
 /*
-example for auth middleware (would go in app.js)
-app.use(async (req, res, next) => {
-    const user = await db.user.findFirst({where: { id:  req.session.userId }})
-    req.user = user
-    next()
-})
+Returns a user and updates if info if needed if it finds a match in the database otherwise it makes a new user, adds it to the database, and returns it
+Matching priority:
+1. google_sub
+2. email
+3. name (first and last)
 */
+async function getOrMakeUser(google_sub, email, given_name, family_name) {
+    let user = await usersCollection.findOne({google_sub: google_sub}); //see if a user exists with their google account
+    if (!user) user = await usersCollection.findOne({email: email}); //see if a user exists with their email
+    if (!user) user = await usersCollection.findOne({name: {first: given_name, last: family_name}}); //see if a user exists with their name
+    if (!user) {
+        user = {
+            // _id generated when .insertOne is called with _id undefined
+            name: {
+                first: given_name,
+                last: family_name
+            },
+            email: email,
+            google_sub: google_sub,
+            roles: [],
+            children: []
+        };
+        await usersCollection.insertOne(user); // insert the user into the collection
+    } else {
+        Object.assign(user, {
+            name: {
+                first: given_name,
+                last: family_name
+            },
+            email: email,
+            google_sub: google_sub
+        });
+        
+        await usersCollection.replaceOne({_id: user._id}, user, {upsert: true}) // replace the user document if it exists, make it if it doesn't
+    }
+    console.log(user);
+    
+    //update user with any new data
 
+    return user;
+}
 module.exports = router;
