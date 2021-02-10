be56cdc9cfb37c7a4661ffd6e9b71ebe4fd3edf6
K3vx21 <k3vn.xu@gmail.com>
2021-02-10 11:58:15 -0600

    start post request function for summary

--- routes/summary.js
+++ routes/summary.js
@@ -2,8 +2,27 @@ let express = require("express");
 
 router = express.Router();
 
+// import the MongoClient class and make a client with the url to our database
+const MongoClient = require('mongodb').MongoClient;
+const uri = `mongodb+srv://admin:${process.env.MONGO_PASSWORD}@cluster0.kfvlj.mongodb.net/merry-tutor?retryWrites=true&w=majority`;
+const mongoClient = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+// connect to the client and pass a reference to the Summaries collection to the global scope so we can use it later
+let summaryCollection;
+mongoClient.connect(err => {
+    if (err) console.log(err);
+    summaryCollection = mongoClient.db("merry-tutor").collection("Summaries");
+    console.log("Connected to Summaries collection");
+});
+
 router.get("/new", (req,res) => {
     res.render("sessionsummary", {});
 });
 
+router.post("/new", (req,res) => {
+    let submission = req.body;
+    summaryCollection.insertOne(submission);
+}) 
+
 module.exports = router;
+
