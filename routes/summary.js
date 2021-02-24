dfefaa7478b5df1176a2fdd1f7323f3f3ceb9c02
K3vx21 <k3vn.xu@gmail.com>
2021-02-24 01:41:40 -0600

    start backend submission processing

--- routes/summary.js
+++ routes/summary.js
@@ -15,14 +15,62 @@ mongoClient.connect(err => {
     console.log("Connected to Summaries collection");
 });
 
-router.get("/new", (req,res) => {
+// connect to the client and pass a reference to the Users collection to the global scope so we can use it later
+let usersCollection;
+mongoClient.connect(err => {
+    if (err) console.log(err);
+    usersCollection = mongoClient.db("merry-tutor").collection("Users");
+    console.log("Connected to Users collection");
+});
+
+router.get("/new", (req, res) => {
     res.render("sessionsummary", {});
 });
 
-router.post("/new", (req,res) => {
-    let submission = req.body;
-    summaryCollection.insertOne(submission); //instead, create new js object format (refer to doc) and THEN insert into collection
-}) 
+router.post("/new", (req, res) => {
+    let formData = req.body; //req.body is a js object of the form
+    console.log(formData);
+    /*  
+    name to id function (author: Dylan Schmit)
+    Makes an async function so you can use await. 
+    Functions that return a promise can have await put before them and code execution will wait for the promise to resolve and 
+    return its value you can make an arrow function async by turning `() => {...}` into `async () => {...}`
+    */
+    /* async */ function find_id (name){
+        let tutor_name = formData["tutor-name"];
+        let [last, first] = name.replace(", ", ",").split(",");
+        // find one doc with name.first being the first name and name.last being the last name. All names are stored in lower case.
+        let userDoc = /*await*/ usersCollection.findOne(
+            { name: { 
+                first: first.toLowerCase(), 
+                last: last.toLowerCase() 
+            }});
+        console.log(userDoc); // log the document of queried user
+        if(userDoc === null){
+            return NaN; // temporary solution for nonexistent users; fix later (02/24/2021)
+        }
+        user_id = userDoc["_id"];
+        console.log(user_id) // log user id
+        return user_id;
+    }
+    // if shadowing tutor is null, set shadow_id as null
+
+    // create js object to be inserted into document
+    let formObj = {
+        date: Date.parse(formData["session-date"]) , // same format as returned by Date.now(),
+        tutor_id: find_id(formData["tutor-name"]), //string
+        tutee_id: find_id(formData["tutee-name"]), //string, // somehow attach parent email
+        shadow_id: find_id(formData["shadow-name"]), //string, (optional)
+        fields: {
+            what_they_learned: formData["what-they-learned"],
+            homework: formData["at-home-suggestion"],
+            next_time: formData["next-session-suggestion"]
+        }
+    }
+    console.log(formObj)
+    summaryCollection.insertOne(formObj); //instead, create new js object format (refer to doc) and THEN insert into collection 
+    console.log("1 document inserted");
+});
 
 module.exports = router;
 
