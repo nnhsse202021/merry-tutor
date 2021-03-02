79ee3936643113e1f0ea6db78d06d13f716bbbcd
K3vx21 <k3vn.xu@gmail.com>
2021-03-02 16:37:46 -0600

    complete session summary feature
    Co-authored-by: cyandev <cyandev@users.noreply.github.com>

--- routes/summary.js
+++ routes/summary.js
@@ -27,40 +27,18 @@ router.get("/new", (req, res) => {
     res.render("sessionsummary", {});
 });
 
-router.post("/new", (req, res) => {
+router.post("/new", async (req, res) => {
     let formData = req.body; //req.body is a js object of the form
     console.log(formData);
-    /*  
-    name to id function (author: Dylan Schmit)
-    Makes an async function so you can use await. 
-    Functions that return a promise can have await put before them and code execution will wait for the promise to resolve and 
-    return its value you can make an arrow function async by turning `() => {...}` into `async () => {...}`
-    */
-    async function find_id (name){
-        let tutor_name = formData["tutor-name"];
-        let [last, first] = name.replace(", ", ",").split(",");
-        // find one doc with name.first being the first name and name.last being the last name. All names are stored in lower case.
-        let userDoc = await usersCollection.findOne(
-            { name: { 
-                first: (first ?? "" ).toLowerCase(),
-                last: (last ?? "").toLowerCase()
-            }});
-        console.log(userDoc); // log the document of queried user
-        if(userDoc === null){
-            return NaN; // temporary solution for nonexistent users; fix later (02/24/2021)
-        }
-        user_id = userDoc["_id"];
-        console.log(user_id) // log user id
-        return user_id;
-    }
+
     // if shadowing tutor is null, set shadow_id as null
 
     // create js object to be inserted into document
     let formObj = {
-        date: Date.parse(formData["session-date"]) , // same format as returned by Date.now(),
-        tutor_id: find_id(formData["tutor-name"]), //string
-        tutee_id: find_id(formData["tutee-name"]), //string, // somehow attach parent email
-        shadow_id: find_id(formData["shadow-name"]), //string, (optional)
+        date: Date.parse(formData["session-date"]), // same format as returned by Date.now(),
+        tutor_id: await find_id(formData["tutor-name"]), //string
+        tutee_id: await find_id(formData["tutee-name"]), //string, // somehow attach parent email
+        shadow_id: await find_id(formData["shadow-name"]), //string, (optional)
         fields: {
             what_they_learned: formData["what-they-learned"],
             homework: formData["at-home-suggestion"],
@@ -68,9 +46,40 @@ router.post("/new", (req, res) => {
         }
     }
     console.log(formObj)
-    summaryCollection.insertOne(formObj); //instead, create new js object format (refer to doc) and THEN insert into collection 
-    console.log("1 document inserted");
+    if(formObj.tutor_id && formObj.tutee_id){    // validate for tutor_id and tutee_id
+        summaryCollection.insertOne(formObj);
+        console.log("1 document inserted");
+        res.redirect("../");
+    } else { // invalid form 
+        // if invalid, alert user, keep form data
+        res.render("sessionsummary", {formData: formData});
+    }
 });
+
+/*  
+    name to id function (author: Dylan Schmit)
+    Makes an async function so you can use await. 
+    Functions that return a promise can have await put before them and code execution will wait for the promise to resolve and 
+    return its value you can make an arrow function async by turning `() => {...}` into `async () => {...}`
+    */
+async function find_id(name) {
+    let [last, first] = name.replace(", ", ",").split(",");
+    // find one doc with name.first being the first name and name.last being the last name. All names are stored in lower case.
+    let userDoc = await usersCollection.findOne(
+        {
+            name: {
+                first: (first ?? "").toLowerCase(),
+                last: (last ?? "").toLowerCase()
+            }
+        });
+    console.log(userDoc); // log the document of queried user
+    if (!userDoc) {
+        return undefined;
+    }
+    user_id = userDoc["_id"];
+    console.log(user_id) // log user id
+    return user_id;
+}
 
 module.exports = router;
 
