c706a9cf6752feda4fe555b16a769a0519260369
cyandev <djs.bitcoin@gmail.com>
2021-03-17 12:15:28 -0500

    fix firstTimeFlow tag breaking animation

--- public/js/login.js
+++ public/js/login.js
@@ -1,5 +1,5 @@
 const emailRegExp = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
-
+var shownSlideId = "base-login";
 /* Google Login Bits */
 gapi.load("auth2", () => { //load the google auth2 api and start it (loaded previous in html, see login.ejs)
     gapi.auth2.init();
@@ -98,7 +98,6 @@ async function doLogin() { //add click listener to #google-login button which wi
 }
 
 /* First Time Login Flow */
-var shownSlideId = "base-login";
 function showSlide(id) {
     console.log(shownSlideId)
     for (let slide of document.querySelector("#slider-box").children) {
