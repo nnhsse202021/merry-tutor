5ffce0af26170b45d99dc5fe95d6d7a446685d82
K3vX21 <54183095+K3vX21@users.noreply.github.com>
2021-03-19 11:39:52 -0500

    remove tutor route

--- routes/tutor.js
+++
@@ -1,9 +0,0 @@
-let express = require("express");
-
-router = express.Router();
-
-router.get("/:name", (req,res) => {
-    res.render("tutor", {user: req.user, name: req.params.name});
-});
-
-module.exports = router;
