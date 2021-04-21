4a9288828abe5a291472819db38f00e32d3284a9
K3vX21 <54183095+K3vX21@users.noreply.github.com>
2021-04-20 23:23:57 -0500

    add board functionality

--- routes/board.js
+++ routes/board.js
@@ -70,6 +70,29 @@ router.post("/removetutor", async (req, res) => {
     res.render("removetutor", { user: req.user, formData: formData});
 });
 
+router.get("/addboard", async (req,res) => {
+    if (!req.user) { //must be logged in to see a tutee's data
+        res.status(401).render("error", {code: 403, description: "You must be logged in to preform this action."});
+        return;
+    } else if (!(req.user.roles.includes("board"))) { //if you are not a board member, you cannot access this data
+        res.status(403).render("error", {code: 403, description: "Unauthoried for logged in user."});
+        return;
+    }
+
+    res.render("addboard", {user: req.user});
+});
+
+router.post("/addboard", async (req, res) => {
+    let formData = req.body;
+    let result = await usersCollection.findOneAndUpdate({email: formData["email"]}, {$push: {roles: "board"}});
+    if (result.value != null){
+        console.log("board role successfully added to tutor with email " + formData["email"] + ".");
+    } else{
+        console.log("Tutor with email " + formData["email"] + "not found.");
+    }
+    res.render("addboard", { user: req.user, formData: formData });
+});
+
 /**
  * Return + update a user if match in database otherwise make a new user, add it to the database and return it
 Matching priority:
