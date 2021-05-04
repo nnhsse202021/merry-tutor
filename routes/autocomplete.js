4e26578f8083dff117ae7dcc03299ac918facda4
Geoff Schmit <gcschmit@gmail.com>
2021-05-03 20:22:56 -0500

    fix typo

--- routes/autocomplete.js
+++ routes/autocomplete.js
@@ -4,12 +4,12 @@ let express = require("express");
 const MongoClient = require('mongodb').MongoClient;
 if(process.env.PRODUCTION) {
 	console.log("Running on production server...");
-	var protocol = "mongodb":
+	var protocol = "mongodb";
 	var mongoHost = "localhost";
 }
 else {
 	console.log("Running for development...");
-	var protocol = "mongodb+srv":
+	var protocol = "mongodb+srv";
 	var mongoHost = "cluster0.kfvlj.mongodb.net";
 }
 const uri = `${protocol}://admin:${process.env.MONGO_PASSWORD}@${mongoHost}/merry-tutor?retryWrites=true&w=majority`;
--- routes/board.js
+++ routes/board.js
@@ -5,12 +5,12 @@ router = express.Router();
 const { MongoClient, ObjectID } = require('mongodb');
 if(process.env.PRODUCTION) {
 	console.log("Running on production server...");
-	var protocol = "mongodb":
+	var protocol = "mongodb";
 	var mongoHost = "localhost";
 }
 else {
 	console.log("Running for development...");
-	var protocol = "mongodb+srv":
+	var protocol = "mongodb+srv";
 	var mongoHost = "cluster0.kfvlj.mongodb.net";
 }
 const uri = `${protocol}://admin:${process.env.MONGO_PASSWORD}@${mongoHost}/merry-tutor?retryWrites=true&w=majority`;
